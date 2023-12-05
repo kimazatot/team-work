2ff34209703e037d7e87f6309f9b7483bcd845a9
test <test@gmail.com>
2023-12-05 14:49:22 +0600

    2nd update

--- teamwork.py
+++ teamwork.py
@@ -1,4 +1,8 @@
-
-
+/bin/python3 /home/acer/Desktop/ade/py32/team-work/team-work/teamwork.py
+/bin/python3 /home/acer/Desktop/ade/py32/team-work/team-work/teamwork.py
+list1 = 5
 new_list = list(map(lambda x: x**2, list1))
 print(new_list)
+
+
+print('my name is aisha')
